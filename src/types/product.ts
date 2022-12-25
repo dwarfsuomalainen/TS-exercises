b26747a8df2c1a24b0138f8a959496ccf9e36b4f
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-12-25 16:52:07 +0200

    40%

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,13 +2,7 @@ import { Entity } from "./common";
 
 export interface Product extends Entity {
 /* complete Product interface with some chosen properties */
-<<<<<<< HEAD
-name: string
-email: string
-password: string
-=======
 title: string
 price: number
->>>>>>> 84cfad7 (40%)
 }
 
